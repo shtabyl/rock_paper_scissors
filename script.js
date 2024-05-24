4103070c767421adef9b96c6759d439f0cd9a956
shtabyl <avnirog@gmail.com>
2024-05-24 11:21:01 +0300

    Style code according to AirbnbStyleGuide

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// Code logic of game "Rock Paper Scissors".
-// Get computer choice, get human choice in each round, 
-// compare results and return winner.
-// Return winner after five rounds.
+/** 
+ * Code logic of game "Rock Paper Scissors".
+ * Get computer choice, get human choice in each round, 
+ * compare results and return a winner of a round.
+ * Return the winner after five rounds.
+ */
 
 const answerOption = ['rock', 'paper', 'scissors'];
 
@@ -17,6 +19,7 @@ function getComputerChoice() {
 function getHumanChoice(round) {
     let input = prompt(`Round ${round}. 
         Enter your choice: rock, paper, scissors?`).toLowerCase();
+
     // Check for wrong spelling in user input
     while (answerOption.indexOf(input) === -1) {
         input = prompt(`Round ${round}. 
@@ -25,68 +28,86 @@ function getHumanChoice(round) {
     return input;
 }
 
-function getScore(roundWinner, score) {
-    if (roundWinner === 'human') {
-        score.human++;
-        return;
-    } else if (roundWinner === 'computer') {
-        score.computer++;
-        return score;
+function getRoundWinner(computerChoice, humanChoice) {
+    let roundWinner;
+    if (computerChoice === 'rock') {
+        switch(humanChoice) {
+            case 'paper': {
+                roundWinner = 'human';
+                break;
+            }
+            case 'scissors': {
+                roundWinner = 'computer';
+                break;
+            }
+            default:{
+                roundWinner = 'tie';
+            }
+        }
+    } else if (computerChoice === 'paper') {
+        switch(humanChoice) {
+            case 'rock': {
+                roundWinner = 'computer';
+                break;
+            }
+            case 'scissors': {
+                roundWinner = 'human';
+                break;
+            }
+            default: {
+                roundWinner = 'tie';
+            }
+        }
     } else {
-        score.computer++;
-        score.human++;
+        switch(humanChoice) {
+            case 'rock': {
+                roundWinner = 'human';
+                break;
+            }
+            case 'paper': {
+                roundWinner = 'computer';
+                break;
+            }
+            default: {
+                roundWinner = 'tie';
+            }
+        }
     }
-    return;
+    return roundWinner;
 }
+
+function getScore(roundWinner, score) {
+        if (roundWinner === 'human') {
+            score.human += 1;
+            return;
+        } else if (roundWinner === 'computer') {
+            score.computer += 1;
+            return score;
+        } else {
+            score.computer += 1;
+            score.human += 1;
+        }
+        return;
+    }
 
 function playRound(score, round) {
     let computerChoice = getComputerChoice();
     let humanChoice = getHumanChoice(round);
     let roundWinner = getRoundWinner(computerChoice, humanChoice);
     getScore(roundWinner, score);
-    console.log(`Round ${round}`)
-    console.log('Computer |', computerChoice, '|', score.computer);
-    console.log('Human:   |', humanChoice, '|', score.human);
+    console.log(`Round ${round}`);
+    
+    // Add spaces after choice presentation for better view
+    let spacesToAdd = 'scissors'.length;
+    let spacesToAddComputer = ' '.repeat(spacesToAdd - computerChoice.length);
+    let spacesToAddHuman = ' '.repeat(spacesToAdd - humanChoice.length);
+    console.log(
+        `Computer | ${computerChoice}${spacesToAddComputer} | ${score.computer}`
+    );
+    console.log(
+        `Human    | ${humanChoice}${spacesToAddHuman} | ${score.human}`
+    );
     return;
-}
-
-function getRoundWinner(computerChoice, humanChoice) {
-    let roundWinner;
-    if (computerChoice === 'rock') {
-        switch(humanChoice) {
-            case 'paper':
-                roundWinner = 'human';
-                break;
-            case 'scissors':
-                roundWinner = 'computer';
-                break;
-            default:
-                roundWinner = 'tie';
-        }
-    } else if (computerChoice === 'paper') {
-        switch(humanChoice) {
-            case 'rock':
-                roundWinner = 'computer';
-                break;
-            case 'scissors':
-                roundWinner = 'human';
-                break;
-            default:
-                roundWinner = 'tie';
-        }
-    } else {
-        switch(humanChoice) {
-            case 'rock':
-                roundWinner = 'human';
-                break;
-            case 'paper':
-                roundWinner = 'computer';
-                break;
-            default:
-                roundWinner = 'tie';
-        }
-    }
-    return roundWinner;
 }
 
 function playGame() {
@@ -100,9 +121,8 @@ function playGame() {
         return 'The winner is Computer!';
     } else if (score.computer < score.human) {
         return 'The winner is Human!';
-    } else {
-        return 'Tie!';
-    }
+    } 
+    return 'Tie!';  
 }
 
 // Add tests for getRoundWinner();
